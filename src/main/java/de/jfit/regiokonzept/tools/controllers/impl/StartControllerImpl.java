1bd9b01b11643b6e19f3ef376c60703e5e2326cc
Jan Furkert <jan.furkert@gmail.com>
2018-12-22 08:37:25 +0100

    PoC Regiokonzept

--- src/main/java/de/jfit/regiokonzept/tools/controllers/impl/StartControllerImpl.java
+++ src/main/java/de/jfit/regiokonzept/tools/controllers/impl/StartControllerImpl.java
@@ -5,7 +5,7 @@
  *
  * Copyright (C) 2018 Jan Furkert IT-Services, All rights reserved.
  */
-package de.jfit.regiokonzept.tools.controllers;
+package de.jfit.regiokonzept.tools.controllers.impl;
 
 import java.util.Map;
 
@@ -19,19 +19,26 @@ import de.jfit.regiokonzept.tools.config.ToolsConfiguration;
  * @author minion69
  */
 @Controller
-public class StartController {
+public class StartControllerImpl {
 
     private ToolsConfiguration toolsConfiguration;
 
     @Autowired
-    public StartController(final ToolsConfiguration toolsConfiguration) {
+    public StartControllerImpl(final ToolsConfiguration toolsConfiguration) {
         super();
         this.toolsConfiguration = toolsConfiguration;
+    }
+
+    @GetMapping("/regiokonzept/greet")
+    public String greet() {
+        return "Hello world!";
     }
 
     @GetMapping("/regiokonzept/tools")
     public String start(Map<String, Object> model) {
         model.put("message", toolsConfiguration.getWelcomeMessage());
+
+        // resources/templates/start.html
         return "start";
     }
 }
--- src/test/java/de/jfit/regiokonzept/tools/controllers/impl/StartControllerTest.java
+++ src/test/java/de/jfit/regiokonzept/tools/controllers/impl/StartControllerTest.java
@@ -1,4 +1,4 @@
-package de.jfit.regiokonzept.tools.controllers;
+package de.jfit.regiokonzept.tools.controllers.impl;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.Mockito.when;
@@ -7,7 +7,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,13 +17,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.util.NestedServletException;
 
 import de.jfit.regiokonzept.tools.config.ToolsConfiguration;
+import de.jfit.regiokonzept.tools.controllers.impl.StartControllerImpl;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(StartController.class)
+@WebMvcTest(StartControllerImpl.class)
 @ActiveProfiles("test")
 public class StartControllerTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,7 +37,21 @@ public class StartControllerTest {
     private ToolsConfiguration toolsconfiguration;
 
     @Test
-    public void testHomeController() throws Exception {
+    public void greet() throws Exception {
+
+        // JUnit 5
+        // assertThrows(TemplateInputException.class, () ->
+        // mockMvc.perform(get("/regiokonzept/greet")).andDo(print()).andExpect(status().isOk())
+        // .andExpect(content().string(containsString("Hello World!"))));
+
+        // JUnit 4
+        thrown.expect(NestedServletException.class /* TemplateInputException.class */);
+        mockMvc.perform(get("/regiokonzept/greet")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World!")));
+    }
+
+    @Test
+    public void start() throws Exception {
 
         when(toolsconfiguration.getWelcomeMessage()).thenReturn("Regiokonzept - Tools");
 
